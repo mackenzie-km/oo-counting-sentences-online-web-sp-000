dc2a3f93dd8c53869a73821ad9f2f51c4bd70219
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-28 20:50:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+   self.end_with?("?")
   end
 
   def exclamation?
-
+   self.end_with?("!")
   end
 
-  def count_sentences
+def count_sentences
+  splitted = self.split(/(?<=[(?!.]\s{1})/)
+  splitted.count{|sentence| sentence? || question? || exclamation?}
+end
 
-  end
 end
