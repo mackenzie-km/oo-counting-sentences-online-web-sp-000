a9d8b91f9b3d67eb96fe58ee9c1a28cc8cd65adf
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-28 20:28:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-  splitted = self.split(/(?<=[(?!.]\s{1})/)
+  splitted = self.split(". " || "! " || "? ")
   splitted.count{|sentence| sentence? || question? || exclamation?}
 end 
 
